125b0b8bf050276310c7f7c1526c322795516e4c
ezzoddin <mobina_ezzoddin@semnan.ac.ir>
2020-11-12 18:53:57 +0330

    update files

--- system/config.php
+++ system/config.php
@@ -1,9 +1,14 @@
 <?php
 
-$base_url = "http://localhost:8080/mvc";
-
+$base_url = "http://localhost/mvc/";
 $base_dir = "/mvc/";
 
 $tmp = explode('?', $_SERVER['REQUEST_URI']);
-$current_route = str_replace($base_dir, '', $tmp[0]);
+$current_route = str_replace($base_dir,'',$tmp[0]);
 unset($tmp);
+
+
+
+
+
+
