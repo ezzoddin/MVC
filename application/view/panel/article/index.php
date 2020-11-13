8be684328b30adf410d7160717a60afd6b83fd6a
ezzoddin <mobina_ezzoddin@semnan.ac.ir>
2020-11-13 23:48:56 +0330

    finish article (maybe)

--- application/view/panel/article/index.php
+++ application/view/panel/article/index.php
@@ -1,7 +1,7 @@
 <?php $this->include("panel.layouts.header"); ?>
 <section class="mb-2 d-flex justify-content-between align-items-center">
     <h2 class="h4">Articles</h2>
-    <a href="create.html" class="btn btn-sm btn-success">Create</a>
+    <a href="<?php $this->url('article/create') ?>" class="btn btn-sm btn-success">Create</a>
 </section>
 
 <section class="table-responsive">
@@ -16,26 +16,19 @@
         </tr>
         </thead>
         <tbody>
-        <tr>
-            <td>1</td>
-            <td>Sport vs Smoke</td>
-            <td>1</td>
-            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</td>
-            <td>
-                <a href="edit.html" class="btn btn-info btn-sm">Edit</a>
-                <a href="#" class="btn btn-danger btn-sm">Delete</a>
-            </td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>global warming</td>
-            <td>2</td>
-            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</td>
-            <td>
-                <a href="edit.html" class="btn btn-info btn-sm">Edit</a>
-                <a href="#" class="btn btn-danger btn-sm">Delete</a>
-            </td>
-        </tr>
+
+        <?php foreach ($articles as $article) { ?>
+            <tr>
+                <td><?= $article['id']; ?></td>
+                <td><?= $article['title']; ?></td>
+                <td><?= $article['cat_id']; ?></td>
+                <td><?= substr($article['body'],0, 40)."..."; ?></td>
+                <td>
+                    <a href="<?php $this->url('article/edit/'/$article['id']); ?>" class="btn btn-info btn-sm">Edit</a>
+                    <a href="<?php $this->url('article/destroy/'/$article['id']); ?>" class="btn btn-danger btn-sm">Delete</a>
+                </td>
+            </tr>
+        <?php } ?>
         </tbody>
     </table>
 </section>
--- application/view/panel/layouts/footer.php
+++ application/view/panel/layouts/footer.php
@@ -1,11 +1,10 @@
-
-            </section>
-        </section>
-    </section>
+</section>
+</section>
+</section>
 
 </section>
 
-    <script src="../js/jquery.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
+<script src="<?php $this->asset('js/jquery.min.js'); ?>"></script>
+<script src="<?php $this->asset('js/bootstrap.min.js'); ?>"></script>
 </body>
 </html>
