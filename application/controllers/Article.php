4d4f03033c0e4e7ff25cf5604ae1728b0d5d9b7c
ezzoddin <mobina_ezzoddin@semnan.ac.ir>
2020-11-24 02:26:08 +0330

    almost finish

--- application/controllers/Article.php
+++ application/controllers/Article.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Application\Controllers;
 
 use Application\Model\Article as ArticleModel;
@@ -9,7 +8,6 @@ use Application\Model\Category;
 
 class Article extends Controller
 {
-
     public function index()
     {
         $article = new ArticleModel();
@@ -19,47 +17,42 @@ class Article extends Controller
 
     public function create()
     {
-        $Category = new Category();
-        $Categories = $Category->all();
-        return $this->view('panel.article.create', compact('Categories'));
+        $category = new Category();
+        $categories = $category->all();
+        return $this->view('panel.article.create', compact('categories'));
     }
 
     public function store()
-    {
-
+    { 
         $article = new ArticleModel();
         $article->insert($_POST);
         return $this->redirect('article');
-
     }
 
     public function show($id)
-    {
-
-    }
+    { }
 
     public function edit($id)
-    {
-        $Category = new Category();
-        $Categories = $Category->all();
+    { 
+        $category = new Category();
+        $categories = $category->all();
         $ob_article = new ArticleModel();
         $article = $ob_article->find($id);
-        return $this->view('panel.article.create', compact('Categories', 'article'));
+        return $this->view('panel.article.edit', compact('categories', 'article'));
     }
 
     public function update($id)
-    {
+    { 
+
         $article = new ArticleModel();
         $article->update($id, $_POST);
         return $this->redirect('article');
     }
 
     public function destroy($id)
-    {
+    { 
         $article = new ArticleModel();
         $article->delete($id);
         return $this->back();
-
     }
-
 }
--- system/bootstrap/Autoload.php
+++ system/bootstrap/Autoload.php
@@ -2,13 +2,16 @@
 
 namespace System\Bootstrap;
 
-class Autoload
-{
-    public function autoloader()
-    {
-        spl_autoload_register(function ($className) {
-            $className = str_replace("\\", DIRECTORY_SEPARATOR, $className);
-            include_once $_SERVER['DOCUMENT_ROOT'] . '/mvc/' . $className . ".php";
-        });
-    }
+
+class Autoload{
+
+public function autoloader(){
+
+    spl_autoload_register(function($className){
+
+      $className = str_replace("\\" , DIRECTORY_SEPARATOR, $className);
+      include_once $_SERVER['DOCUMENT_ROOT'] . '/mvc/' . $className . '.php';
+
+    });
+}
 }
